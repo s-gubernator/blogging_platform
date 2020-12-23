9b4d69393363507a2c6e1242fe9a474170a69184
s-gubernator <svitlana.gubernator@yahoo.com>
2020-12-23 21:09:56 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+User.destroy_all
+
+25.times do
+  first_name = Faker::Name.unique.first_name
+  last_name  = Faker::Name.unique.last_name
+  email      = Faker::Internet.unique.safe_email
+
+  User.create!(first_name: first_name,
+               last_name: last_name,
+               email: email)
+end
+
+p "Created #{User.count} users"

3c9b213e645b99fc0c025bbaa7e3b44a263928cc
s-gubernator <svitlana.gubernator@yahoo.com>
2020-12-24 13:55:57 +0200

    Fix rubocop offenses

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
@@ -29,21 +31,21 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'rubocop', '~> 1.6', require: false
+gem 'rubocop-performance', require: false
 gem 'rubocop-rails', require: false
 gem 'rubocop-rspec', require: false
-gem 'rubocop-performance', require: false
 
-gem 'strong_migrations'
-gem 'slim'
 gem 'annotate'
+gem 'slim'
+gem 'strong_migrations'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'pry', '~> 0.13.1'
-  gem 'rspec-rails', '~> 4.0.1'
   gem 'factory_bot_rails'
   gem 'faker', git: 'https://github.com/faker-ruby/faker.git', branch: 'master'
+  gem 'pry', '~> 0.13.1'
+  gem 'rspec-rails', '~> 4.0.1'
 end
 
 group :development do
@@ -51,17 +53,17 @@ group :development do
   gem 'web-console', '>= 4.1.0'
   # Display performance information such as SQL time and flame graphs for each request in your browser.
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
-  gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
+  gem 'rack-mini-profiler', '~> 2.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
   gem 'brakeman'
+  gem 'spring'
 end
 
 group :test do
-  gem 'simplecov', require: false
   gem 'shoulda-matchers', '~> 4.0'
+  gem 'simplecov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: users
@@ -28,24 +30,25 @@ RSpec.describe User, type: :model do
   describe 'user validations' do
     subject { user }
 
-    it { should validate_presence_of(:first_name) }
-    it { should validate_length_of(:first_name).is_at_most(50) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_length_of(:first_name).is_at_most(50) }
 
-    it { should validate_presence_of(:last_name) }
-    it { should validate_length_of(:last_name).is_at_most(50) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_length_of(:last_name).is_at_most(50) }
 
-    it { should validate_presence_of(:email) }
-    it { should validate_length_of(:email).is_at_most(255) }
-    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { (user.email).should match(URI::MailTo::EMAIL_REGEXP) }
   end
 
   describe 'test user without email' do
     let(:user_without_email) { build(:user, first_name: 'Test', last_name: 'User', email: nil) }
 
+    it { expect(user_without_email).to be_valid }
+
     it 'set default email' do
       user_without_email.save
-      expect(user_without_email).to be_valid
       expect(user_without_email.email).to eq('test.user@example.com')
     end
   end
@@ -53,7 +56,7 @@ RSpec.describe User, type: :model do
   describe '#full_name' do
     let(:test_user) { create(:user, first_name: 'Test', last_name: 'User') }
 
-    it { should respond_to(:full_name) }
+    it { is_expected.to respond_to(:full_name) }
 
     it 'collects Full Name from first_name and last_name' do
       expect(test_user.full_name).to eq('Test User')

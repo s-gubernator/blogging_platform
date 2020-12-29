7398390d579ea4009154baf8d749d3056ac23cf1
s-gubernator <svitlana.gubernator@yahoo.com>
2020-12-29 17:26:37 +0200

    Add imports to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,8 @@ import "regenerator-runtime/runtime"
 import "materialize-css/dist/js/materialize"
 
 import "stylesheets/application"
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+require("@rails/ujs").start()

8ef206bbcb2f25e324652750a858223e57748178
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-04 14:19:38 +0200

    Fix according to code review

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-        address: "localhost",
-        port: 1025,
-        enable_starttls_auto: false
-    }
+  config.action_mailer.smtp_settings = {
+      address: "localhost",
+      port: 1025,
+      enable_starttls_auto: false
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

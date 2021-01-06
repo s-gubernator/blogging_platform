ae5bf329d9564a181285c05846ac531414c19d08
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-06 12:05:47 +0200

    Initial commit for administration namespace

---
+++ app/controllers/administration/users_controller.rb
@@ -0,0 +1,2 @@
+class Administration::UsersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :admin do
+    resources :users
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

0dd05f3317fffe68464c44afe5ec2e454eda3b19
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-06 19:26:05 +0200

    Add spec for DashboardPolicy

---
+++ spec/policies/dashboard_policy_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DashboardPolicy do
+  subject(:dashboard_policy) { described_class }
+
+  let(:simple_user) { build(:user) }
+  let(:admin_user) { build(:user, :admin) }
+
+  permissions :admin? do
+    it 'denies access for simple user' do
+      expect(dashboard_policy).not_to permit(simple_user)
+    end
+
+    it 'grants access for admin user' do
+      expect(dashboard_policy).to permit(admin_user)
+    end
+  end
+end

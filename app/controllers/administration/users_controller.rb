757342eab0f3ef7a2bec5d17e2e5f40d44d71406
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-06 13:00:10 +0200

    Change addministration view

--- app/controllers/administration/users_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Administration
-  class UsersController < ApplicationController
-    def index
-      @users = User.all
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :administration do
-    resources :users
+    get '/', to: 'dashboard#home'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

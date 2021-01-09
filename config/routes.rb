8ff473710e420d3191e64b39cac5c9b40b0c6965
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-09 20:47:47 +0200

    Remove default error pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     root 'dashboard#home'
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+  match '/422', to: 'errors#internal_server_error', via: :all
 end

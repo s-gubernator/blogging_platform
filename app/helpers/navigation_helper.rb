1a479a93e166d01749df6dc27a3293ff5c558e92
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-12 21:46:39 +0200

    Move nav_link method to Navigation helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module NavigationHelper
+  def main_nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    tag.li(class: class_name) do
+      link_to(link_text, link_path)
+    end
+  end
+
   def nav_items
     @nav_items ||= [{ title: 'Home', url: '/' }]
   end

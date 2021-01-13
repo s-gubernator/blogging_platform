438ab4ee423250019b1fddb8ffc6af2fea924141
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-13 14:01:33 +0200

    Create administration folder for policies spec

--- spec/controllers/administration/users_controller_spec.rb
+++ spec/controllers/administration/users_controller_spec.rb
@@ -24,18 +24,17 @@ RSpec.describe Administration::UsersController, type: :controller do
   end
 
   describe 'DELETE /destroy' do
-    subject!(:delete_action) { delete :destroy, params: { id: user.id } }
-
     context 'when user with role "admin" is logged' do
       login_admin
-      subject(:delete_action_lambda) { -> { delete :destroy, params: { id: user.id } } }
+      subject(:delete_action) { delete :destroy, params: { id: user.id } }
 
-      it { expect(delete_action_lambda).to change(User, :count).by(-1) }
+      it { expect { delete_action }.to change(User, :count).by(-1) }
       it { expect(delete_action).to redirect_to(administration_users_url) }
     end
 
     context 'when user with role "simple" is logged' do
       login_simple
+      subject(:delete_action) { delete :destroy, params: { id: user.id } }
 
       it { expect(delete_action).to have_http_status(:unauthorized) }
     end

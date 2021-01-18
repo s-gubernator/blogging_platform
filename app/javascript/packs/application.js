8b7c462bc4f8dbbb617e0a7c37e0e60e6ea4c067
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-18 13:07:19 +0200

    Remove unneeded scaffold from lib/templated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,9 @@ require("@rails/ujs").start()
 document.addEventListener('DOMContentLoaded', () => {
   M.AutoInit();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  let noticeEl = document.querySelector('#notice');
+
+  if (noticeEl) {  M.toast( { html: noticeEl.dataset.notice, displayLength: 5000 })}
+})

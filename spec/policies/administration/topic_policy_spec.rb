afefa1edab75c4bdb7293ca4a5743152b74cced4
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-20 15:07:07 +0200

    Fix policies for topics

--- spec/policies/administration/topic_policy_spec.rb
+++ spec/policies/administration/topic_policy_spec.rb
@@ -8,14 +8,8 @@ describe TopicPolicy do
   let(:simple_user) { build(:user) }
   let(:admin_user) { build(:user, :admin) }
 
-  permissions :index?, :show? do
-    it 'grants access for all visitors' do
-      expect(topic_policy).to permit(nil)
-    end
-  end
-
   context 'when visitor go to page' do
-    permissions :new?, :create?, :edit?, :update?, :destroy? do
+    permissions :index?, :show?, :new?, :create?, :edit?, :update?, :destroy? do
       it 'prohibits visitors access' do
         expect(topic_policy).not_to permit(nil)
       end
@@ -23,7 +17,7 @@ describe TopicPolicy do
   end
 
   context 'when admin user is logged' do
-    permissions :new?, :create?, :edit?, :update?, :destroy? do
+    permissions :index?, :show?, :new?, :create?, :edit?, :update?, :destroy? do
       it 'permits admin access' do
         expect(topic_policy).to permit(admin_user)
       end
@@ -31,13 +25,7 @@ describe TopicPolicy do
   end
 
   context 'when simple user is logged' do
-    permissions :new?, :create? do
-      it 'grant access' do
-        expect(topic_policy).to permit(simple_user)
-      end
-    end
-
-    permissions :edit?, :update?, :destroy? do
+    permissions :index?, :show?, :new?, :create?, :edit?, :update?, :destroy? do
       it 'denies access' do
         expect(topic_policy).not_to permit(simple_user)
       end

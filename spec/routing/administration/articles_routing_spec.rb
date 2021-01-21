8a778975fa7215e6537d2613fa7984ed3fb9f14b
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-21 15:40:50 +0200

    Fix spec file naming

--- spec/routing/administration/articles_routing_spec.rb
+++ spec/routing/administration/articles_routing_spec.rb
@@ -2,38 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe Administration::TopicsController, type: :routing do
+RSpec.describe Administration::ArticlesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: 'administration/topics').to route_to('administration/topics#index')
+      expect(get: 'administration/articles').to route_to('administration/articles#index')
     end
 
     it 'routes to #show' do
-      expect(get: 'administration/topics/1').to route_to('administration/topics#show', id: '1')
-    end
-
-    it 'routes to #new' do
-      expect(get: 'administration/topics/new').to route_to('administration/topics#new')
-    end
-
-    it 'routes to #edit' do
-      expect(get: 'administration/topics/1/edit').to route_to('administration/topics#edit', id: '1')
-    end
-
-    it 'routes to #create' do
-      expect(post: 'administration/topics').to route_to('administration/topics#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: 'administration/topics/1').to route_to('administration/topics#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: 'administration/topics/1').to route_to('administration/topics#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: 'administration/topics/1').to route_to('administration/topics#destroy', id: '1')
+      expect(get: 'administration/articles/1').to route_to('administration/articles#show', id: '1')
     end
   end
 end

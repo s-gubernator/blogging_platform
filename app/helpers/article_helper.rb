6376cfe6c5dc99b44e1b11dee62774f6ef8bf7c5
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-25 21:03:27 +0200

    Add admin approved to articles

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -12,6 +12,14 @@ module ArticleHelper
     }
   end
 
+  def status_icon(article)
+     article.approved ?  'check' : 'remove'
+  end
+
+  def status_class(article)
+    article.approved ?  'success' : 'danger'
+  end
+
   private
 
   def helpers
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,7 @@
 # Table name: articles
 #
 #  id         :bigint           not null, primary key
+#  approved   :boolean          default(FALSE)
 #  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null

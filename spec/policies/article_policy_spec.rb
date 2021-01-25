2429a65541f81f3a8b5dc374e8d494710001f507
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-25 16:39:21 +0200

    Fix rubocop offenses

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -9,29 +9,39 @@ describe ArticlePolicy do
   let(:last_article) { author.articles.last }
   let(:simple_user) { create(:user) }
 
-  permissions :user_articles?, :create?, :new? do
-    it 'denies access for visitor' do
-      expect(article_policy).not_to permit(nil)
-    end
-
-    it 'grants access for author' do
-      expect(article_policy).to permit(author)
+  context 'when author go to page' do
+    permissions :user_articles?, :visitor_show?, :create?, :new?, :update?, :edit?, :destroy? do
+      it 'grants access for author' do
+        expect(article_policy).to permit(author, last_article)
+      end
     end
   end
 
-  permissions :visitor_show? do
-    it 'grants access for all users' do
-      expect(article_policy).to permit(nil)
+  context 'when visitor go to page' do
+    permissions :visitor_show? do
+      it 'grants access for all users' do
+        expect(article_policy).to permit(nil)
+      end
+    end
+
+    permissions :user_articles?, :create?, :new?, :update?, :edit?, :destroy? do
+      it 'denies access for visitor' do
+        expect(article_policy).not_to permit(nil, last_article)
+      end
     end
   end
 
-  permissions :update?, :edit?, :destroy? do
-    it 'denies access for simple user' do
-      expect(article_policy).not_to permit(simple_user, last_article)
+  context 'when simple user go to page' do
+    permissions :user_articles?, :visitor_show?, :create?, :new? do
+      it 'grants access for simple user' do
+        expect(article_policy).to permit(simple_user)
+      end
     end
 
-    it 'grants access for author' do
-      expect(article_policy).to permit(author, last_article)
+    permissions :update?, :edit?, :destroy? do
+      it 'denies access for simple user' do
+        expect(article_policy).not_to permit(simple_user, last_article)
+      end
     end
   end
 end

281b422551953ff1045917f6b282d86bc03395cb
s-gubernator <svitlana.gubernator@yahoo.com>
2021-01-26 18:07:40 +0200

    Add check all js for articles approve

--- app/javascript/packs/administration.js
+++ app/javascript/packs/administration.js
@@ -6,3 +6,14 @@ import "stylesheets/administration"
 
 require("@rails/ujs").start()
 require("packs/shared")
+
+document.addEventListener('DOMContentLoaded', () => {
+  let selectAll = document.getElementById('selectAll');
+
+  selectAll.addEventListener('click', () => {
+    let checkboxes = document.querySelectorAll('input[type=checkbox]');
+    for (var i = 0; i < checkboxes.length; i++) {
+     checkboxes[i].checked = true;
+    };
+  });
+});

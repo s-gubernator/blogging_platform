87385a163382f9999bd804d07e5fa0f1cf0df5ab
s-gubernator <svitlana.gubernator@yahoo.com>
2021-02-07 19:39:21 +0200

    Add styles to ShowCount

--- app/javascript/components/ShowCount.js
+++ app/javascript/components/ShowCount.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import './ShowCount.css';
 
 class ShowCount extends React.Component {
   constructor(props) {
@@ -35,9 +36,6 @@ class ShowCount extends React.Component {
 
   render() {
     return (
-      // <div style={{
-      //    position: 'absolute', left: '50%', top: '50%',
-      //    transform: 'translate(-50%, -50%)' }}>
       <div>
         <p>Current Count: { this.state.count }</p>
         <button className="inc" onClick={ (e) => this.increment(e) }>Increment!</button>

0ee0c9f0039d7a51ee94b26ac31f782b48c0ba9b
s-gubernator <svitlana.gubernator@yahoo.com>
2021-02-08 20:57:07 +0200

    Finish article component

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -8,8 +8,15 @@ class Article extends React.Component {
         <div className='article-title'>
           <a href={this.props.path}>{this.props.title}</a>
         </div>
-        <div className='article-content'>
+        <div className='article-body'>
           {this.props.content}
+          <div className='article-meta-details'>
+            Created by: {this.props.author}, {this.props.created_at} ago,
+            <br/ >last updated: {this.props.updated_at} ago
+            <p className='article-topic'>
+              {this.props.topic}
+            </p>
+          </div>
         </div>
       </React.Fragment>
     );
@@ -20,5 +27,8 @@ Article.propTypes = {
   title: PropTypes.string,
   path: PropTypes.string,
   content: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string,
 };
+
 export default Article
